147034b95381ddc22e7b59131f2e436dde39def0
Mihyun Jun <jjunmmee@gmail.com>
2023-08-09 09:41:55 +0900

    fix mongo setting

---
+++ lion_app/blog/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from pymongo import MongoClient
+
+MONGO_HOST = os.getenv("MONGO_HOST", "mongo")
+client = MongoClient(host=MONGO_HOST)
+db = client.likelion
+
+
+class BlogSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=100)
+    content = serializers.CharField()
+    author = serializers.CharField(max_length=100)
+
+    def create(self, validated_data):
+        return db.blogs.insert_one(validated_data)
+    
+    def save(self, **kwargs):
+        # Find item
+        # if exists()
+        # update
+        # else
+        # create
+        
+        return 
+    
