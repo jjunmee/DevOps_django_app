fb76805e840a88c9a22d71f1031c7de232c05ad8
Mihyun Jun <jjunmmee@gmail.com>
2023-08-09 10:43:41 +0900

    forum

---
+++ lion_app/Forum/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Topic, Post
+
+
+class TopicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Topic
+        fields = "__all__"
+        read_only_fields = (
+            "id",
+            "created_at",
+            "updated_at",
+        )
