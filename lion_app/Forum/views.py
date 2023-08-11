1a21bc5b82d6367b97171dd838ac096546e1f5b4
Mihyun Jun <jjunmmee@gmail.com>
2023-08-11 11:17:47 +0900

    .

--- lion_app/Forum/views.py
+++ lion_app/Forum/views.py
@@ -1,14 +1,17 @@
+from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
 
 from .models import Topic, Post
 from .serializers import TopicSerializer, PostSerializer
 
 
+@extend_schema(tags=["Topic"])
 class TopicViewSet(viewsets.ModelViewSet):
     queryset = Topic.objects.all()
     serializer_class = TopicSerializer
 
 
+@extend_schema(tags=["Post"])
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
