4485be288e5cde65ed839cab82a727f4ac8676db
Mihyun Jun <jjunmmee@gmail.com>
2023-08-11 11:20:37 +0900

    Forum.views: fix schema

--- lion_app/Forum/views.py
+++ lion_app/Forum/views.py
@@ -10,6 +10,9 @@ class TopicViewSet(viewsets.ModelViewSet):
     queryset = Topic.objects.all()
     serializer_class = TopicSerializer
 
+    @extend_schema(summary="새 토픽 생성")
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
 
 @extend_schema(tags=["Post"])
 class PostViewSet(viewsets.ModelViewSet):
